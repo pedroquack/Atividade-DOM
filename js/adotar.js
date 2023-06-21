ed35f1b1501a51afe06da83f772ced63c2a6c979
pedroquack <pedrovolpattocosta@gmail.com>
2023-06-21 15:50:51 -0300

    Exibindo erros pro usuario

--- js/adotar.js
+++ js/adotar.js
@@ -1,11 +1,49 @@
 let formulario = document.querySelector("form")
-formulario.addEventListener("submit",(event) => {
-    event.preventDefault();
-    let nome = document.querySelector("#nome").value;
-    let telefone = document.querySelector("#telefone").value;
-    let email = document.querySelector("#email").value;
-    let renda = document.querySelector("input[type='radio']:checked").value;
+
+const check_inputs = (nome, telefone, email) => {
+    let control = true;
+    if (nome.value.trim() == "") {
+        nome.classList.add("erro");
+        control = false;
+    }else{
+        nome.classList.remove("erro");
+    }
+    if (telefone.value.trim() == "") {
+        telefone.classList.add("erro");
+        control = false;
+    }else{
+        telefone.classList.remove("erro");
+    }
+    if (email.value.trim() == "") {
+        email.classList.add("erro");
+        control = false;
+    }else{
+        email.classList.remove("erro");
+    }
+
+    return control;
+}
+
+formulario.addEventListener("submit", (event) => {
+    let nome = document.querySelector("#nome");
+    let telefone = document.querySelector("#telefone");
+    let email = document.querySelector("#email");
+    let renda = document.querySelector("input[type='radio']:checked");
     let residencia = document.querySelector("#residencia");
-    let opcao_residencia = residencia.options[residencia.selectedIndex].value;
-    console.log(opcao_residencia);
+    let opcao_residencia = residencia.options[residencia.selectedIndex];
+    let intencao = document.querySelector("#intencao");
+
+    if (!check_inputs(nome, telefone, email)) {
+        event.preventDefault();
+    } else {
+        const adotante = {
+            id: 1,
+            nome: nome.value,
+            telefone: telefone.value,
+            email: email.value,
+            renda: renda.value,
+            residencia: opcao_residencia.value,
+            intencao: intencao.value,
+        };
+    }
 });
